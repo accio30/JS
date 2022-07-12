c3afd8624ac4aed21eb3deb8b99dbf090816f54e
accio30 <jeronimocastrocastro@gmail.com>
2022-07-12 17:35:00 -0500

    Optimus

--- index.js
+++ index.js
@@ -2,42 +2,49 @@ let homenum =document.getElementById("guest-points-text")
 let guestnum = document.getElementById("home-points-text")
 let hsetsnum = document.getElementById("HSETS")
 let gsetsnum = document.getElementById("GSETS")
+let timenum = document.getElementById("TIMER")
 let homechange = 0
 let guestchange = 0
 let gsetschange = 0
 let hsetschange = 0
+var timechange=30
 
-function addshome1(){
-    homechange += 1
-    homenum.textContent = homechange
-}
 
-function addshome2(){
-    homechange += 2
-    homenum.textContent = homechange
-}
-
-function addshome3(){
-    homechange += 3
-    homenum.textContent = homechange
-}
-
-function addsguest1(){
-    guestchange += 1
-    guestnum.textContent = guestchange
-}
-
-function addsguest2(){
-    guestchange += 2
-    guestnum.textContent = guestchange
-}
-
-function addsguest3(){
-    guestchange += 3
-    guestnum.textContent = guestchange
-}
-
-function newgame(){
+function func(val){
+switch(val){
+    case 1:
+        homechange += 1
+        homenum.textContent = homechange   
+    break
+    case 2:
+        homechange += 2
+        homenum.textContent = homechange 
+    break
+    case 3:
+        homechange += 3
+        homenum.textContent = homechange   
+    break
+    case 4:
+        guestchange += 1
+        guestnum.textContent = guestchange  
+    break
+    case 5:
+        guestchange += 2
+        guestnum.textContent = guestchange  
+    break
+    case 6:
+        guestchange += 3
+        guestnum.textContent = guestchange  
+    break   
+    case 7:
+        hsetschange += 1
+        hsetsnum.textContent = hsetschange
+    break   
+    case 8:
+        gsetschange += 1
+        gsetsnum.textContent = gsetschange
+    break   
+    case 9:
     guestchange = 0
     homechange = 0
     hsetschange = 0
@@ -47,17 +54,13 @@ function newgame(){
     hsetsnum.textContent = hsetschange
     guestnum.textContent = guestchange
     homenum.textContent = homechange
+    break
+    case 10:
+        timechange -= 1
+        timenum.textContent = timechange
+        
+    break 
+    
 }
-
-function GSETSS(){
-    gsetschange += 1
-    gsetsnum.textContent = gsetschange
 }
-
-function HSETSS(){
-    hsetschange += 1
-    hsetsnum.textContent = hsetschange
-}
-
-
 
